f92b63c8218e1c63d349d143efa5cb03982fab36
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2023-11-17 21:33:21 +0100

    emotes now work but certain messages can be out of order

--- logic.js
+++ logic.js
@@ -1,35 +1,43 @@
-document.addEventListener('onEventReceived', function (obj) {
-
-	var badges = document.getElementsByClassName("badges");
-	var lastBadge = badges[badges.length - 1];
-	
-	var nodes = lastBadge.childNodes;
-	
-	for(var i = 0; i < nodes.length; i++)
-	{
-		nodes[i].remove();
-	}
-
-
-
-
-
-
+document.addEventListener('onEventReceived', function (obj) 
+{
 	var msgs = document.getElementsByClassName("message");
-	typingEffect(msgs[msgs.length - 1],50);
+    var lastMsg = msgs[msgs.length - 1];
+    typingEffect(lastMsg,50);
 });
 
-function typingEffect(element,speed){
-  let text=element.innerHTML;
-  element.innerHTML="";
- var i=0;
-  var timer=setInterval(function(){
-    if(i<text.length){
-      element.append(text.charAt(i))
-      i++;
-    }else{
-      clearInterval(timer);
+function typingEffect(element,speed)
+{
+    let elementClone = element.cloneNode(true);
+    let nodes = elementClone.childNodes;
+    
+    element.innerHTML = "";
+    
+    for (let i = 0; i < nodes.length; i++) 
+    {
+        let node = nodes[i];
+
+        if (node.nodeName.includes("text"))
+        {
+            console.log(nodes.length);
+            let text=node.data;
+            var j=0;
+            var timer=setInterval(function()
+                {
+                    if(j<text.length)
+                    {
+                        element.append(text.charAt(j))
+                        j++;
+                    }
+                    else
+                    {
+                        clearInterval(timer);
+                    }
+                },speed);
+        }
+        else
+        {
+            element.appendChild(node);
+            console.log(nodes.length);
+        }
     }
-  },speed)
-  
 }
