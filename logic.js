da0e0dc127017350479870777b529ed29466c788
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2023-11-24 19:33:33 +0100

    now badges appear as a text instead of images

--- logic.js
+++ logic.js
@@ -1,11 +1,64 @@
 document.addEventListener('onEventReceived', function (obj) 
 {
+    AddBadgesToName();
+
 	var msgs = document.getElementsByClassName("message");
     var lastMsg = msgs[msgs.length - 1];
-    typingEffect(lastMsg,50);
+    TypingEffect(lastMsg,50);
 });
 
-function typingEffect(element,speed)
+function AddBadgesToName()
+{
+    var metas = document.querySelectorAll(".meta");
+	var lastMeta = metas[metas.length - 1];
+    
+    var badges = GetBadges(lastMeta);
+    if (badges == null) return;
+
+    var badgesName = GetBadgesSlashConcatName(badges);
+    
+    var originalName = GetElementUserName(lastMeta).innerText;
+
+    GetElementUserName(lastMeta).innerText = badgesName + originalName;
+
+    GetBadgesWrapper(lastMeta).remove();
+}
+
+function GetElementUserName(lastMeta)
+{
+    return lastMeta.querySelector(".name");
+}
+
+function GetBadgesWrapper(lastMeta)
+{
+    return lastMeta.querySelector(".badges");
+}
+
+function GetBadges(lastMeta)
+{
+    var badgesWrapper = GetBadgesWrapper(lastMeta);
+    if (badgesWrapper != null)
+    {
+        return badgesWrapper.childNodes;
+    }
+
+    return null;
+}
+
+function GetBadgesSlashConcatName(badges)
+{
+    var badgeName = "";
+    for (let i = 0; i < badges.length; i++)
+    {
+        let originalName = badges[i].className;
+        let split1 = originalName.split(" ")[1];
+        let split2 = split1.split("-")[0];
+        badgeName += split2 + "/";
+    }
+    return badgeName;
+}
+
+function TypingEffect(element,speed)
 {
     let elementClone = element.cloneNode(true);
     let nodes = elementClone.childNodes;
@@ -18,7 +71,6 @@ function typingEffect(element,speed)
 
         if (node.nodeName.includes("text"))
         {
-            console.log(nodes.length);
             let text=node.data;
             var j=0;
             var timer=setInterval(function()
@@ -37,7 +89,6 @@ function typingEffect(element,speed)
         else
         {
             element.appendChild(node);
-            console.log(nodes.length);
         }
     }
 }
